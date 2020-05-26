ceb0f03930d814dcf3b8abe085aec459af407f4b
Michael Davidsaver <mdavidsaver@gmail.com>
2020-05-25 17:07:51 -0700

    CurrentRecord

--- mrfCommon/src/devObj.cpp
+++ mrfCommon/src/devObj.cpp
@@ -1,4 +1,8 @@
 
+#include <stdexcept>
+
+#include <epicsThread.h>
+
 #include "devObj.h"
 
 const
@@ -14,3 +18,29 @@ linkOptionDef objdef[] =
     linkString  (addrBase, parent , "PARENT"  , 0, 0),
     linkOptionEnd
 };
+
+static
+struct CurrentID_t {
+    epicsThreadPrivateId id;
+    CurrentID_t()
+        :id(epicsThreadPrivateCreate())
+    {
+        if(!id)
+            throw std::bad_alloc();
+    }
+} CurrentID;
+
+void CurrentRecord::set(dbCommon* prec)
+{
+    epicsThreadPrivateSet(CurrentID.id, prec);
+}
+
+CurrentRecord::~CurrentRecord()
+{
+    set(0);
+}
+
+dbCommon* CurrentRecord::get()
+{
+    return (dbCommon*)epicsThreadPrivateGet(CurrentID.id);
+}
